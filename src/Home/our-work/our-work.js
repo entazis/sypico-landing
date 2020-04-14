1c58d355f00545496149a30ac6d8f47156bafa7c
Szabó Bence <szabo.bence.tat@gmail.com>
2020-04-14 15:56:09 +0200

    our-work style

--- src/Home/our-work/our-work.js
+++ src/Home/our-work/our-work.js
@@ -1,52 +1,92 @@
 import React from 'react';
+import {Container, Row, Col, Carousel} from 'react-bootstrap';
+import { FaRegCheckCircle } from 'react-icons/all';
 
 import classes from './our-work.module.css';
-import {Container, Row, Col} from 'react-bootstrap';
 
 function ourWork() {
   return (
       <section id='our-work' className={classes.ourWork}>
-        <Container>
-          <Row>
-            <Col lg={6}>
-              <img alt='project' className={classes.leftImage} src={'https://trynext-react.envytheme.com/_next/static/images/it-service3-e0d514d60a8e95d29c308e081837482a.png'}/>
-            </Col>
-            <Col lg={6}>
-              <div className={classes.infoBlock}>
-                <div className={classes.infoHead}>
-                  <h2 className="title">Digital Marketing</h2>
-                  <p>Lorem ipsum dolor sit amet, consectetur
-                  adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-                  dolore magna aliqua. Ut enim ad minim veniam, quis nostrud
-                  exercitation ullamco laboris.</p></div>
-              </div>
+        <div className={classes.sectionTitle}>
+          <h2>Our Projects</h2>
+          <p>We help businesses automate boring repetitive tasks!</p>
+          <a className="btn btn-danger" href="#contact">Get In Touch</a>
+        </div>
+        <Carousel>
+          <Carousel.Item>
+            <Container>
               <Row>
-                <Col sm={6}>
-                  <div className={classes.featureList}>
-                    <i className='icofont-check-circled'/> SEO Marketing
-                  </div>
-                  <div className={classes.featureList}>
-                    <i className='icofont-check-circled'/> Facebook Marketing
-                  </div>
-                  <div className={classes.featureList}>
-                    <i className='icofont-check-circled'/> Social Marketing
-                  </div>
+                <Col lg={6}>
+                  <img alt='project' className={classes.leftImage} src={'https://trynext-react.envytheme.com/_next/static/images/it-service3-e0d514d60a8e95d29c308e081837482a.png'}/>
                 </Col>
-                <Col sm={6}>
-                  <div className={classes.featureList}>
-                    <i className='icofont-check-circled'/> Email Marketing
+                <Col lg={6}>
+                  <div className={classes.infoBlock}>
+                    <div className={classes.infoHead}>
+                      <h2 className="title">Data Analytics</h2>
+                      <p>It became clear that price setting in the house renting business is crucial but also rather complex as it is very dependent on season and events.
+                        We took it upon us to crunch the numbers and analyze their data in detail. Our Data Analytics resulted in a revenue increase of 10%.</p>
+                    </div>
                   </div>
-                  <div className={classes.featureList}>
-                    <i className='icofont-check-circled'/> Data Scraping
-                  </div>
-                  <div className={classes.featureList}>
-                    <i className='icofont-check-circled'/> Youtube Marketing
-                  </div>
+                  <Row>
+                    <Col sm={6}>
+                      <div className={classes.featureList}>
+                        <FaRegCheckCircle/> Airbnb
+                      </div>
+                      <div className={classes.featureList}>
+                        <FaRegCheckCircle/> Data Analytics
+                      </div>
+
+                    </Col>
+                    <Col sm={6}>
+                      <div className={classes.featureList}>
+                        <FaRegCheckCircle/> Setting Prices
+                      </div>
+                      <div className={classes.featureList}>
+                        <FaRegCheckCircle/> Increase in revenue
+                      </div>
+                    </Col>
+                  </Row>
                 </Col>
               </Row>
-            </Col>
-          </Row>
-        </Container>
+            </Container>
+          </Carousel.Item>
+          <Carousel.Item>
+            <Container>
+              <Row>
+                <Col lg={6}>
+                  <div className={classes.infoBlock}>
+                    <div className={classes.infoHead}>
+                      <h2 className="title">Automation tool</h2>
+                      <p>Master-planning list is automatically updated every five minutes. Thanks to SyPiCo no more precious time is wasted and resources are used much more efficiently.</p>
+                    </div>
+                  </div>
+                  <Row>
+                    <Col sm={6}>
+
+                      <div className={classes.featureList}>
+                        <FaRegCheckCircle/> Mobile App Development
+                      </div>
+                      <div className={classes.featureList}>
+                        <FaRegCheckCircle/> React Web Development
+                      </div>
+                    </Col>
+                    <Col sm={6}>
+                      <div className={classes.featureList}>
+                        <FaRegCheckCircle/> UI / UX Design
+                      </div>
+                      <div className={classes.featureList}>
+                        <FaRegCheckCircle/> Responsive Design
+                      </div>
+                    </Col>
+                  </Row>
+                </Col>
+                <Col lg={6}>
+                  <img alt='project' className={classes.leftImage} src={'https://trynext-react.envytheme.com/_next/static/images/it-service1-22e4b3cb0d0c5e883d34ab51829a8cca.png'}/>
+                </Col>
+              </Row>
+            </Container>
+          </Carousel.Item>
+        </Carousel>
       </section>
   );
 }
