daaaa5b209f17c6b9af5da7b795539a0a40b5f62
Szabó Bence <szabo.bence.tat@gmail.com>
2020-04-14 10:34:08 +0200

    our-work section

--- src/Home/our-work/our-work.js
+++ src/Home/our-work/our-work.js
@@ -9,7 +9,7 @@ function ourWork() {
         <Container>
           <Row>
             <Col lg={6}>
-              <img className={classes.leftImage} src={'https://trynext-react.envytheme.com/_next/static/images/it-service3-e0d514d60a8e95d29c308e081837482a.png'}/>
+              <img alt='project' className={classes.leftImage} src={'https://trynext-react.envytheme.com/_next/static/images/it-service3-e0d514d60a8e95d29c308e081837482a.png'}/>
             </Col>
             <Col lg={6}>
               <div className={classes.infoBlock}>
@@ -21,8 +21,28 @@ function ourWork() {
                   exercitation ullamco laboris.</p></div>
               </div>
               <Row>
-                <Col sm={6}></Col>
-                <Col sm={6}></Col>
+                <Col sm={6}>
+                  <div className={classes.featureList}>
+                    <i className='icofont-check-circled'/> SEO Marketing
+                  </div>
+                  <div className={classes.featureList}>
+                    <i className='icofont-check-circled'/> Facebook Marketing
+                  </div>
+                  <div className={classes.featureList}>
+                    <i className='icofont-check-circled'/> Social Marketing
+                  </div>
+                </Col>
+                <Col sm={6}>
+                  <div className={classes.featureList}>
+                    <i className='icofont-check-circled'/> Email Marketing
+                  </div>
+                  <div className={classes.featureList}>
+                    <i className='icofont-check-circled'/> Data Scraping
+                  </div>
+                  <div className={classes.featureList}>
+                    <i className='icofont-check-circled'/> Youtube Marketing
+                  </div>
+                </Col>
               </Row>
             </Col>
           </Row>
