b42371f57f47e6f8f45e64e5ba3a5cbe42c8e21c
Szabó Bence <szabo.bence.tat@gmail.com>
2020-04-14 10:47:31 +0200

    contact section

--- src/Home/contact/contact.js
+++ src/Home/contact/contact.js
@@ -1,14 +1,25 @@
 import React from 'react';
 
 import classes from './contact.module.css';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container} from 'react-bootstrap';
 
 function contact() {
   return (
       <section className={classes.contact}>
-        Contact
+        <Container>
+          <div className={classes.contactInnerArea}>
+            <h2>Contact Us</h2>
+            <form className="newsletter-form">
+              <input type="email"
+                     className="form-control"
+                     placeholder="Enter your email"
+                     name="email" required=""/>
+                <button className={classes.formSubmissionButton} type="submit">Subscribe!</button>
+            </form>
+          </div>
+        </Container>
       </section>
-  );
+);
 }
 
 export default contact;
