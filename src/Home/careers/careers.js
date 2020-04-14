94edf2de558f99dda92a80d6bd2b30c7e0950a0c
Szabó Bence <szabo.bence.tat@gmail.com>
2020-04-14 11:13:08 +0200

    careers section

--- src/Home/careers/careers.js
+++ src/Home/careers/careers.js
@@ -1,12 +1,49 @@
 import React from 'react';
 
 import classes from './careers.module.css';
-import {Container, Row, Col} from 'react-bootstrap';
+import {Container, Row, Col, Button} from 'react-bootstrap';
 
 function careers() {
   return (
       <section className={classes.careers}>
-        Careers
+        <Container>
+          <div className={classes.sectionTitle}>
+            <h2>We're looking for new talent!</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+          </div>
+          <Row className="justify-content-md-center">
+            <Col lg={4} sm={6}>
+              <div className={classes.singleTable}>
+                <div className={classes.tableHeader}><h3>Internship</h3></div>
+                <div className={classes.tablePrice}><sup>$</sup>120 <sub>/ Day</sub></div>
+                <ul className={classes.tableFeatures}>
+                  <li><i className="icofont-check-circled">*</i> Web Development
+                  </li>
+                  <li><i className="icofont-check-circled">*</i> Email Integration
+                  </li>
+                  <li><i className="icofont-check-circled">*</i> Messenger
+                    Integration
+                  </li>
+                  <li><i className="icofont-check-circled">*</i> Mobile + Desktop
+                    Apps
+                  </li>
+                  <li><i className="icofont-check-circled">*</i> Quick Responses
+                  </li>
+                  <li><i className="icofont-check-circled">*</i> Drag &amp; Drop
+                    Widgets
+                  </li>
+                  <li><i className="icofont-check-circled">*</i> Visitor Notes
+                  </li>
+                  <li><i className="icofont-check-circled">*</i> Google Analytics
+                  </li>
+                </ul>
+                <div className="btn-box">
+                  <Button variant='danger'>Apply job</Button>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
       </section>
   );
 }
