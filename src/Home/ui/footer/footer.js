ead780c1ee252c7bbf2e24ecb4952ef1b1410fee
Szabó Bence <szabo.bence.tat@gmail.com>
2020-04-17 07:45:20 +0200

    footer updates

--- src/Home/ui/footer/footer.js
+++ src/Home/ui/footer/footer.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FaFacebook, FaTwitter, FaLinkedin, FaInstagram, FaHome, FaPhone, FaEnvelope } from 'react-icons/all';
 
 import classes from './footer.module.css';
 import {Container, Row, Col} from 'react-bootstrap';
@@ -16,54 +17,66 @@ const footer = () => {
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
               </p>
               <ul className={classes.socialLinks}>
-                <li><a target="_blank" href=""><i
-                    className="icofont-facebook"></i></a></li>
-                <li><a target="_blank" href=""><i
-                    className="icofont-twitter"></i></a></li>
-                <li><a target="_blank" href=""><i
-                    className="icofont-linkedin"></i></a></li>
-                <li><a target="_blank" href=""><i
-                    className="icofont-instagram"></i></a></li>
+                <li className={classes.socialLink}>
+                  <a target="_blank" rel="noreferrer noopener" href="https://www.facebook.com/gabriel.mattys">
+                    <div style={{color: 'white'}}>
+                      <FaFacebook/>
+                    </div>
+                  </a>
+                </li>
+                <li className={classes.socialLink}>
+                  <a target="_blank" rel="noreferrer noopener" href="https://twitter.com/">
+                    <div style={{color: 'white'}}>
+                      <FaTwitter/>
+                    </div>
+                  </a></li>
+                <li className={classes.socialLink}>
+                  <a target="_blank" rel="noreferrer noopener" href="https://www.linkedin.com/in/gabriel-mattys/">
+                    <div style={{color: 'white'}}>
+                      <FaLinkedin/>
+                    </div>
+                  </a>
+                </li>
+                <li className={classes.socialLink}>
+                  <a target="_blank" rel="noreferrer noopener" href="https://www.instagram.com/">
+                    <div style={{color: 'white'}}>
+                      <FaInstagram/>
+                    </div>
+                  </a>
+                </li>
               </ul>
             </Col>
             <Col lg={3} md={6} sm={6}>
               <h3>Explore</h3>
-              <ul className="list">
-                <li><a href="#home">Home</a></li>
-                <li><a href="#about">About</a></li>
-                <li><a href="#services">Services</a></li>
-                <li><a href="#team">Team</a></li>
-                <li><a href="#our-work">Our work</a></li>
-                <li><a href="#testimonials">Testimonials</a></li>
-                <li><a href="#contact">Contact</a></li>
-                <li><a href="#careers">Careers</a></li>
+              <ul className={classes.linkList}>
+                <li><a className={classes.link} href="#home">Home</a></li>
+                <li><a className={classes.link} href="#about">About</a></li>
+                <li><a className={classes.link} href="#services">Services</a></li>
+                <li><a className={classes.link} href="#team">Team</a></li>
+                <li><a className={classes.link} href="#our-work">Our work</a></li>
+                <li><a className={classes.link} href="#testimonials">Testimonials</a></li>
+                <li><a className={classes.link} href="#contact">Contact</a></li>
+                <li><a className={classes.link} href="#careers">Careers</a></li>
               </ul>
             </Col>
             <Col lg={3} sm={6}>
               <h3>Quick Links</h3>
-              <ul className="list">
-                <li><a href="#contact">Contact Us</a></li>
-                <li><a href="/privacy-policy/">Privacy Policy</a></li>
-                <li><a href="/terms-conditions/">Terms &amp; Conditions</a></li>
+              <ul className={classes.linkList}>
+                <li><a className={classes.link} href="#contact">Contact Us</a></li>
+                <li><a className={classes.link} href="/privacy-policy/">Privacy Policy</a></li>
+                <li><a className={classes.link} href="/terms-conditions/">Terms &amp; Conditions</a></li>
               </ul>
             </Col>
             <Col lg={3} sm={6}>
               <h3>Get in Touch</h3>
               <ul className={classes.getInTouch}>
-                <li><i className="icofont-home"></i> 2750 Quadra Street
-                  Victoria, Canada.
-                </li>
-                <li><i className="icofont-live-support"></i> +324-9442-515</li>
-                <li><i className="icofont-envelope"></i> hello@trynext.com</li>
+                <li><FaHome/> Tramweglei 74, 2500 Lier</li>
+                <li><FaPhone/> +324-9442-515</li>
+                <li><a href='mailto:info@sipyco.com'><FaEnvelope/> info@sipyco.com</a></li>
               </ul>
             </Col>
           </Row>
         </Container>
-        <div className={classes.copyright}>
-          <Container>
-            <p>© Copyright 2020 Pungent. All Rights Reserved By <a target="_blank" href="https://envytheme.com">EnvyTheme</a>.</p>
-          </Container>
-        </div>
       </div>
   );
 };
