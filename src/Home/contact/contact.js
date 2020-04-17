e567a82fc8c2cfa70050099d60d559a395b7bc0b
Szabó Bence <szabo.bence.tat@gmail.com>
2020-04-17 10:05:03 +0200

    contact form update

--- src/Home/contact/contact.js
+++ src/Home/contact/contact.js
@@ -1,25 +1,46 @@
 import React from 'react';
 
 import classes from './contact.module.css';
-import {Container} from 'react-bootstrap';
+import {Container, Row, Col} from 'react-bootstrap';
 
 function contact() {
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(event.target.name.value);
+    console.log(event.target.email.value);
+    console.log(event.target.message.value);
+  };
+
   return (
       <section id='contact' className={classes.contact}>
         <Container>
           <div className={classes.contactInnerArea}>
             <h2>Contact Us</h2>
-            <form className="newsletter-form">
-              <input type="email"
-                     className="form-control"
-                     placeholder="Enter your email"
-                     name="email" required=""/>
-                <button className={classes.formSubmissionButton} type="submit">Subscribe!</button>
+            <form onSubmit={handleFormSubmit}>
+              <Container>
+                <Row className='mb-3'>
+                  <Col md={6}>
+                    <label>Name</label>
+                    <input name='name' type='text' className='form-control' />
+                  </Col>
+                  <Col md={6}>
+                    <label>Email</label>
+                    <input name='email' type='text' className='form-control' />
+                  </Col>
+                </Row>
+                <Row className='mb-3'>
+                  <Col>
+                    <label>Message</label>
+                    <textarea name='message' className='form-control' rows='5'/>
+                  </Col>
+                </Row>
+                <button type='submit' className='btn btn-danger'>Submit</button>
+              </Container>
             </form>
           </div>
         </Container>
       </section>
-);
+  );
 }
 
 export default contact;
