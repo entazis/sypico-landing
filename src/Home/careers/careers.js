f3173602499efc34cb2eec16b72654e7395da359
Szabó Bence <szabo.bence.tat@gmail.com>
2020-04-17 08:15:55 +0200

    career icon, style update

--- src/Home/careers/careers.js
+++ src/Home/careers/careers.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import classes from './careers.module.css';
 import {Container, Row, Col, Button} from 'react-bootstrap';
+import { FaCheckDouble } from 'react-icons/all';
 
 function careers() {
   return (
@@ -12,32 +13,21 @@ function careers() {
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
           </div>
           <Row className="justify-content-md-center">
-            <Col lg={4} sm={6}>
+            <Col lg={5} sm={6}>
               <div className={classes.singleTable}>
-                <div className={classes.tableHeader}><h3>Internship</h3></div>
-                <div className={classes.tablePrice}><sup>$</sup>120 <sub>/ Day</sub></div>
+                <div className={classes.tableHeader}>Internship</div>
+                <div className={classes.tablePrice}>Software Developer</div>
                 <ul className={classes.tableFeatures}>
-                  <li><i className="icofont-check-circled">*</i> Web Development
-                  </li>
-                  <li><i className="icofont-check-circled">*</i> Email Integration
-                  </li>
-                  <li><i className="icofont-check-circled">*</i> Messenger
-                    Integration
-                  </li>
-                  <li><i className="icofont-check-circled">*</i> Mobile + Desktop
-                    Apps
-                  </li>
-                  <li><i className="icofont-check-circled">*</i> Quick Responses
-                  </li>
-                  <li><i className="icofont-check-circled">*</i> Drag &amp; Drop
-                    Widgets
-                  </li>
-                  <li><i className="icofont-check-circled">*</i> Visitor Notes
-                  </li>
-                  <li><i className="icofont-check-circled">*</i> Google Analytics
-                  </li>
+                  <li><FaCheckDouble/> Web Development</li>
+                  <li><FaCheckDouble/> Email Integration</li>
+                  <li><FaCheckDouble/> Messenger Integration</li>
+                  <li><FaCheckDouble/> Mobile + Desktop Apps</li>
+                  <li><FaCheckDouble/> Quick Responses</li>
+                  <li><FaCheckDouble/> Drag &amp; Drop Widgets</li>
+                  <li><FaCheckDouble/> Visitor Notes</li>
+                  <li><FaCheckDouble/> Google Analytics</li>
                 </ul>
-                <div className="btn-box">
+                <div className="btn-box text-center">
                   <Button variant='danger'>Apply job</Button>
                 </div>
               </div>
