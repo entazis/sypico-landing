b90cc961338a9861a70df1da58366b049b5f7d46
Szabó Bence <szabo.bence.tat@gmail.com>
2020-04-17 10:05:56 +0200

    navbar element order update, decreased service pb

--- src/Home/ui/navbar/navbar.js
+++ src/Home/ui/navbar/navbar.js
@@ -21,11 +21,11 @@ export default () => {
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className={['ml-auto', classes.navText]} activeKey={location.hash}>
               <Nav.Link href="#home">Home</Nav.Link>
-              <Nav.Link href="#about">About</Nav.Link>
               <Nav.Link href="#services">Services</Nav.Link>
-              <Nav.Link href="#team">Team</Nav.Link>
               <Nav.Link href="#our-work">Our Work</Nav.Link>
+              <Nav.Link href="#team">Team</Nav.Link>
               <Nav.Link href="#testimonials">Testimonials</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
               <Nav.Link href="#contact">Contact</Nav.Link>
               <Nav.Link href="#careers">Careers</Nav.Link>
             </Nav>
