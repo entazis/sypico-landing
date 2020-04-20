a63cd9a8cd2a801a8f303b672a949f6ecf4102c6
Szabó Bence <szabo.bence.tat@gmail.com>
2020-04-20 10:08:12 +0200

    localized about section

--- src/Home/about/about.js
+++ src/Home/about/about.js
@@ -1,7 +1,8 @@
 import React from 'react';
-
 import classes from './about.module.css';
 import {Container, Row, Col} from 'react-bootstrap';
+import {FormattedMessage} from 'react-intl';
+import messages from '../../localization/base';
 
 function about() {
   return (
@@ -11,23 +12,23 @@ function about() {
             <Col lg={7} md={12}>
               <div className="about-content about-content-two pl-0 mt-0">
                 <div className="section-title">
-                  <h2>About Our Business</h2>
-                  <h4>Automate, keep going and going!</h4>
+                  <h2><FormattedMessage {...messages.aboutTitle}/></h2>
+                  <h4><FormattedMessage {...messages.aboutSubtitle}/></h4>
                 </div>
                 <div className="about-text">
-                  <p>Have you ever wondered how much time you waste on watching parameters several times a day to gather them into reports? Do you get any happier whilst processing data from multiple documents and trying your hardest to combine them in a clearly structured overview? Or even worse, how does it make you feel to browse the same web page over and over again?</p>
+                  <p><FormattedMessage {...messages.aboutP1}/></p>
                 </div>
                 <div className="about-text">
-                  <p>These tasks may be very crucial to your business but nevertheless we all know that they are not the most satisfying.</p>
+                  <p><FormattedMessage {...messages.aboutP2}/></p>
                 </div>
                 <div className="about-text">
-                  <p>I remember my starting job as a Tax Consultant. One of my first projects required me to perform some benchmark analyses. In part of this process it was my job to research the web in order to datamine the needed information. Unfortunately for me this was just a fancy way of saying that I had to click on 200 websites to find one certain parameter. Needless to say this job did not make feel satisfied at all. I knew I was contributing to our project but that didn’t make me feel any more valuable or happy.</p>
+                  <p><FormattedMessage {...messages.aboutP3}/></p>
                 </div>
                 <div className="about-text">
-                  <p>This is exactly where we, at SyPiCo, step in. We can help you to collect your needed information and to organize and analyze these date. After all, we’d love to make sure you could go to bed feeling all happy and satisfied. All you have to do is to get in touch with us and let our automation processes do the rest.</p>
+                  <p><FormattedMessage {...messages.aboutP4}/></p>
                 </div>
                 <div className="about-text">
-                  <p>At SyPiCo we want to keep high achievers as yourself motivated. We allow you to hand over your most boring and repetitive tasks, as we make sure that automation takes over. With our knowledge and software we can solve even the most complex issues of your organization.</p>
+                  <p><FormattedMessage {...messages.aboutP5}/></p>
                 </div>
               </div>
             </Col>
